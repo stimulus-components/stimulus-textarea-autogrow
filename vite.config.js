342d2c9168ece17c47fee5f42bfbcfed51812e9a
Guillaume Briday <guillaumebriday@gmail.com>
2022-12-25 23:30:38 -0500

    Bump to Vite 4 and adding lib name

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import path from 'path'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-export default ({ mode }) => {
-  if (mode === 'netlify') return {}
+export default defineConfig(({ mode }) => {
+  if (mode === 'netlify') {
+    return {}
+  }
 
   return {
     build: {
       lib: {
-        entry: path.resolve(__dirname, 'src/index.ts'),
-        name: 'stimulus-textarea-autogrow'
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'StimulusTextareaAutogrow',
+        fileName: 'stimulus-textarea-autogrow'
       },
       rollupOptions: {
         external: ['@hotwired/stimulus'],
@@ -19,4 +23,4 @@ export default ({ mode }) => {
       }
     }
   }
-}
+})
